d3b2e94aecd5bd16ee34a948b0213e14dd7b7e94
Filip Söderquist <filip.soderquist@gmail.com>
2021-05-18 10:56:03 +0200

    added changes

---
+++ demo_lightgbm.py
@@ -0,0 +1,53 @@
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+from sklearn import preprocessing
+from preprocessing import prepross_fun
+import lightgbm as lgb
+import numpy as np
+from data_loading.data_prep_and_load import load_d, bin_label
+
+
+def main():
+
+    path="C:/Users/filip/OneDrive/Exjobb/data_collection/data_ordered/"
+
+    x,y=load_d(path)
+
+    y=bin_label(y,lable=1)
+
+    feats=prepross_fun.filter_n_DE(x[:,128*3:,:])
+
+    kf= KFold(5,shuffle=False)
+
+    param = { 'objective':'binary','max_bin': 512,'verbose': -1,'first_metric_only': True}
+    param['metric'] = ['binary_error','auc','binary_logloss']
+    num_round = 1000
+    preds=np.zeros((30,5))
+    aucs=np.zeros((30,5))
+
+    for j in range(2,30):
+        param['num_leaves']=j
+        
+        for i,(train_index,test_index) in enumerate(kf.split(feats)):
+                scaler=preprocessing.MinMaxScaler().fit(feats[train_index])
+
+                x_train=scaler.transform(feats[train_index])
+                x_test=scaler.transform(feats[test_index])
+
+                train_data=lgb.Dataset(x_train,label=y[train_index])
+                valid_data=lgb.Dataset(x_test,label=y[test_index])
+
+                bst = lgb.train(param, train_data, num_round, valid_sets=[valid_data], early_stopping_rounds=10, verbose_eval=False)
+                # bst.save_model('model'+str(i)+'.txt', num_iteration=bst.best_iteration)
+                
+                ypred = bst.predict(x_test)
+                preds[j-2,i]=((ypred>0.5).astype(int)==y[test_index]).sum()/y[test_index].shape[0]   
+                aucs[j-2,i]=roc_auc_score(y[test_index],ypred)
+
+        print("Mean acc: "+ str(preds[j-2].mean()),"Mean AUC: "+ str(aucs[j-2].mean()),"Num leaves: " +str(j))
+    print("Best round acc: "+str(preds.mean(axis=1).max()),
+    "Best round AUC: "+ str(aucs.mean(axis=1)[preds.mean(axis=1).argmax()]),
+     "Num leaves: "+str(preds.mean(axis=1).argmax()+2))
+
+if __name__ =='__main__':
+    main()
---
+++ demo_nn.py
@@ -0,0 +1,41 @@
+
+from data_loading import data_prep_and_load
+from preprocessing import prepross_fun
+from training_n_evaluation.train_n_eval import train_kfold, majority_voting  
+import numpy as np
+from sklearn.model_selection import KFold
+from models import models
+
+
+def main():
+    path="C:/Users/filip/OneDrive/Exjobb/data_collection/data_ordered/"
+
+    x,y=data_prep_and_load.load_d(path)
+    
+    x = prepross_fun.bp_n_cat(x)
+    #Likeability
+    y = data_prep_and_load.bin_label(y,lable=0)
+    
+    kf= KFold(5,shuffle=False)
+ 
+    gen= kf.split(x)
+    indices=list(zip(*gen))
+    no_class=1
+
+    model=models.EEG_net(seq_len=1280,in_ch=70, bottle_neck_size=20,filters=32, num_classes=no_class)
+
+    all_samp_acc=np.zeros(5)
+    all_maj_acc=np.zeros(5)
+    for i in range(5):
+        model,x_val,y_val= train_kfold(i,indices,x,y,model,ss=0,os=1,aug=0)
+
+        maj_acc,samp_acc=majority_voting(model,x_val,y_val,num_classes=no_class,split=5)
+
+        print(maj_acc,samp_acc,i)
+        all_maj_acc[i]=maj_acc
+        all_samp_acc[i]=samp_acc
+
+    print(all_maj_acc.mean(),all_samp_acc.mean())
+
+if __name__ =='__main__':
+    main()
